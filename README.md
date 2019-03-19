eb5b95a63a9b8ad98f84be3c9244e73e7f40a694
stanma <malkin.stanislav@gmail.com>
2019-03-19 13:08:43 +0200

    final version

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Test: `npm test`
 
+If using minikube: `curl localhost/trees -H Host:local.ecosia.org`
+
 Deploy: run `/bin/sh deploy.sh` and it will deploy app to k8s
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/stanma/ecosia/master/ecosia.yaml
+kubectl create ns production
+kubectl --namespace=production apply -f https://raw.githubusercontent.com/stanma/ecosia/master/ecosia.yaml
